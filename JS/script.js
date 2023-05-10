49a4c9fa0fbbd7013225ffd3c4e7ee78ea42eeda
EdoardaDepine <edoarda.depine@gmail.com>
2023-05-10 08:25:55 -0300

    criando função para criar e  exibir os elementos na tela do usuário

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,28 @@
-import { getProductsAPI } from './fetchProducts'
+//GET ITENS FROM API:
+async function getProductsAPI() {
+    const response = await fetch(
+        `https://api.mercadolibre.com/sites/MLB/search?q=$computador`
+    );
+    const products = await response.json();
+    return products;
+}
+const products = await getProductsAPI();
+const productsList = products.results;
+const insertionItens = document.querySelector(".itens");
+console.log(productsList);
 
-console.log(getAllComputers());
+//CREATE ELEMENT PRODUCT:
+function createProductItemElement(arrayProducts) {
+    return arrayProducts.map((product) => {
+        insertionItens.innerHTML += `
+    <div class=product>
+    <img class="img-product" src="${product.thumbnail}"/>
+    <h1>${product.title}</h1>
+    <p>${product.price}</p>
+    <button class="button-add-product">Adicionar ao carrinho</button>
+    </div>
+    `;
+    });
+}
+createProductItemElement(productsList);
+console.log(createProductItemElement(productsList));
